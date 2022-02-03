09a9cff82ee0d58033e9d5e27582ba82452bcb23
Sagi <sagibarshai1@gmail.com>
2022-02-03 22:18:38 +0200

    adding data info for menu

---
+++ src/components/Data/allMenu.jsx
@@ -0,0 +1,146 @@
+  export const food = {
+      //fish
+    fish:{
+        salmon:{
+                name:'',
+                img: <img src="images/menu/salmon.jpg" alt="Salmon" />,
+                description: '',
+                price: ''},
+        redTona:{
+                name:'',
+                img: <img sec="images/menu/redTona.jpg" alt="Red tuna"/>,
+                description: '',
+                price: ''
+            },
+        danis:{
+                name:'',
+                img: <img sec="images/menu/danis.jpg" alt="danis"/>,
+                description: '',
+                price: ''
+            }
+        },
+              //fish
+                //
+              //meat 
+    meat:{
+            fillet:{
+                name:'',
+                img: <img src="images/menu/fillet.jpg" alt="fillet" />,
+                description: '',
+                price: ''},
+            burger:{
+                    name:'',
+                    img: <img src="images/menu/burger.jpg" alt="burger" />,
+                    description: '',
+                    price: ''},
+            chickenSnacks:{
+                        name:'',
+                        img: <img src="images/menu/chickenSnacks.jpg" alt="chicken Snacks" />,
+                        description: '',
+                        price: ''},
+            entrecote:{
+                            name:'',
+                            img: <img src="images/menu/entrecote.jpg" alt="entrecote" />,
+                            description: '',
+                            price: ''},
+            meatballs:{  
+                                name:'',
+                                img: <img src="images/menu/meatballs.jpg" alt="meatballs"/>,
+                                description: '',
+                                price: ''},
+            thai:{              
+                                name:'',    
+                                img: <img src="images/menu/thai.jpg" alt="Salmon" />,
+                                description: '',
+                                price: ''},
+
+                            },
+                             //meat 
+    pizza:{
+            white:{
+                                    name:'',
+                                    img:<img src="images/menu/white.jpg" alt="white pizza"/>,
+                                    description: '',
+                                    price: ''
+
+                                },
+            pepperoni:{
+                                    name:'',
+                                    img:<img src="images/menu/pepperoni.jpg" alt="pepperoni pizza"/>,
+                                    description: '',
+                                    price: ''
+
+                                },
+            margarita:{
+                                    name:'',
+                                    img:<img src="images/menu/margarita.jpg" alt="margarita pizza"/>,
+                                    description: '',
+                                    price: ''
+
+                                },
+                                pasta:{
+            alfredo:{
+                                        name:'',
+                                    img:<img src="images/menu/alfredo-pasta.jpg" alt="alfredo pasta"/>,
+                                    description: '',
+                                    price: ''              
+                                    },
+            tomato:{
+                                        name:'',
+                                    img:<img src="images/menu/tomato-pasta.jpg" alt="tomato pasta"/>,
+                                    description: '',
+                                    price: ''   
+                                    },
+            alfredo:{
+                                        name:'',
+                                    img:<img src="images/menu/bolognese-pasta.jpg" alt="bolognese pasta"/>,
+                                    description: '',
+                                    price: ''
+                                    },
+                                }
+
+                            
+                            },
+    desserts:{
+            cheeseCake:{
+                                    name:'',
+                                    img: <img src="images/menu/cheese-cake.jpg" alt="cheese cake"/>,
+                                    description:'',
+                                    price:''
+                                },
+            caramalCake:{
+                                    name:'',
+                                    img: <img src="images/menu/caramel-cake.jpg" alt="caramel cake"/>,
+                                    description:'',
+                                    price:''
+                                },
+            chocolateCake:{
+                                    name:'',
+                                    img: <img src="images/menu/chocolate-cake.jpg" alt="chocolate cake"/>,
+                                    description:'',
+                                    price:''
+                                },
+            macaroon:{
+                                    name:'',
+                                    img: <img src="images/menu/macaroon.jpg" alt="macaroon"/>,
+                                    description:'',
+                                    price:''
+                                },
+            mousse:{
+                                    name:'',
+                                    img: <img src="images/menu/mousse.jpg" alt="mousse"/>,
+                                    description:'',
+                                    price:''
+                                },
+            muffin:{
+                                    name:'',
+                                    img: <img src="images/menu/muffin.jpg" alt="muffin"/>,
+                                    description:'',
+                                    price:''
+                                },
+                                
+                            }
+
+
+        }
+    
