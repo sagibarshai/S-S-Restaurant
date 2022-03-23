fcc4538ce39ba616fec567040e4b7ee01fb69f3f
Sagi <sagibarshai1@gmail.com>
2022-03-23 13:04:47 +0200

    finish

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import NavBarHomePage from './components/layout/NavBarHomePage';
 import NavBarMenuPage from './components/layout/NavBarMenuPage';
 import Footer from './components/layout/Footer';
 import {Switch,Route,Redirect} from 'react-router-dom'
+import NotFound from './components/layout/NotFound';
 import FoodMenu from './components/menu-display/FoodMenu';
 function App() {  
   return (
     <div>
-      {/* <Route>
-        <Redirect to="/home-page" exact/>
-      </Route> */}
+   
+        <Route path="/">
+        <Redirect to="/home-page" />
+      </Route>
       <Switch>
 <Route path="/home-page" exact>
       <NavBarHomePage/>
@@ -21,13 +23,12 @@ function App() {
     <ImageSection/>
     <Footer/>
 </Route>
-      </Switch>
-      <Switch>
       <Route path="/menu" exact>
         <NavBarMenuPage/>
       <FoodMenu/>
       <Footer/>
       </Route>
+        <Route path="*"><NotFound/></Route>
       </Switch>
     </div>
   );
---
+++ src/components/layout/NotFound.jsx
@@ -0,0 +1,6 @@
+import React from "react"
+import '../../sass/Layout/not-found.scss'
+const NotFound = () => {
+return <div className="not-found">Page Not Found ..</div>
+}
+export default NotFound;
